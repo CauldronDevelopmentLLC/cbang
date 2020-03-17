0f09a1fed4887dd98bc2f351aaea8a1f88bb693a
Joseph Coffland <joseph@cauldrondevelopment.com>
2020-03-16 19:24:16 -0700

    Fix ConcurrentPool event handling

--- src/cbang/event/ConcurrentPool.cpp
+++ src/cbang/event/ConcurrentPool.cpp
@@ -46,7 +46,7 @@ bool ConcurrentPool::Task::shouldShutdown() {
 
 ConcurrentPool::ConcurrentPool(cb::Event::Base &base, unsigned size) :
   ThreadPool(size), base(base),
-  event(base.newEvent(this, &ConcurrentPool::complete, 0)) {
+  event(base.newEvent(this, &ConcurrentPool::complete, EF::EVENT_NO_SELF_REF)) {
   if (!Base::threadsEnabled())
     THROW("Cannot use Event::ConcurrentPool without threads enabled.  "
           "Call Event::Base::enableThreads() before creating Event::Base.");
