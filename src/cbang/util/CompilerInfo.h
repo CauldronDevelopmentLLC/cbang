c98094598a9b416f9fd1a1de45e39ceff013d595
Terry Mahaffey <terrymah@microsoft.com>
2020-05-20 20:26:27 -0400

    Update MSVC version strings, so the world doesn't think we're compiling with VS 2008

--- src/cbang/util/CompilerInfo.h
+++ src/cbang/util/CompilerInfo.h
@@ -48,6 +48,46 @@
 #elif __GNUC__
 #define COMPILER "GNU " __VERSION__
 
+#elif _MSC_VER >= 1928
+#define COMPILER "Visual C++"
+#elif _MSC_VER >= 1927
+#define COMPILER "Visual C++ 2019 16.7"
+#elif _MSC_VER >= 1926
+#define COMPILER "Visual C++ 2019 16.6"
+#elif _MSC_VER >= 1925
+#define COMPILER "Visual C++ 2019 16.5"
+#elif _MSC_VER >= 1924
+#define COMPILER "Visual C++ 2019 16.4"
+#elif _MSC_VER >= 1923
+#define COMPILER "Visual C++ 2019 16.3"
+#elif _MSC_VER >= 1922
+#define COMPILER "Visual C++ 2019 16.2"
+#elif _MSC_VER >= 1921
+#define COMPILER "Visual C++ 2019 16.1"
+#elif _MSC_VER >= 1920
+#define COMPILER "Visual C++ 2019 16.0"
+#elif _MSC_VER >= 1916
+#define COMPILER "Visual C++ 2017 15.9"
+#elif _MSC_VER >= 1915
+#define COMPILER "Visual C++ 2017 15.8"
+#elif _MSC_VER >= 1914
+#define COMPILER "Visual C++ 2017 15.7"
+#elif _MSC_VER >= 1913
+#define COMPILER "Visual C++ 2017 15.6"
+#elif _MSC_VER >= 1912
+#define COMPILER "Visual C++ 2017 15.5"
+#elif _MSC_VER >= 1911
+#define COMPILER "Visual C++ 2017 15.3"
+#elif _MSC_VER >= 1910
+#define COMPILER "Visual C++ 2017 15.0"
+#elif _MSC_VER >= 1900
+#define COMPILER "Visual C++ 2015"
+#elif _MSC_VER >= 1800
+#define COMPILER "Visual C++ 2013"
+#elif _MSC_VER >= 1700
+#define COMPILER "Visual C++ 2012"
+#elif _MSC_VER >= 1600
+#define COMPILER "Visual C++ 2010"
 #elif _MSC_VER >= 1500
 #define COMPILER "Visual C++ 2008"
 #elif _MSC_VER >= 1400
