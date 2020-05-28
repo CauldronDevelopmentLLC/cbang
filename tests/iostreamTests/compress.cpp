9e792bced9912c861683bacb4fe7b1ffd65ebd32
Joseph Coffland <joseph@cauldrondevelopment.com>
2020-05-28 15:24:58 -0700

    Added auto (de)compress file support

---
+++ tests/iostreamTests/compress.cpp
@@ -0,0 +1,54 @@
+/******************************************************************************\
+
+          This file is part of the C! library.  A.K.A the cbang library.
+
+                Copyright (c) 2003-2019, Cauldron Development LLC
+                   Copyright (c) 2003-2017, Stanford University
+                               All rights reserved.
+
+         The C! library is free software: you can redistribute it and/or
+        modify it under the terms of the GNU Lesser General Public License
+       as published by the Free Software Foundation, either version 2.1 of
+               the License, or (at your option) any later version.
+
+        The C! library is distributed in the hope that it will be useful,
+          but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+                 Lesser General Public License for more details.
+
+         You should have received a copy of the GNU Lesser General Public
+                 License along with the C! library.  If not, see
+                         <http://www.gnu.org/licenses/>.
+
+        In addition, BSD licensing may be granted on a case by case basis
+        by written permission from at least one of the copyright holders.
+           You may request written permission by emailing the authors.
+
+                  For information regarding this software email:
+                                 Joseph Coffland
+                          joseph@cauldrondevelopment.com
+
+\******************************************************************************/
+
+#include <cbang/Catch.h>
+#include <cbang/os/SystemUtilities.h>
+
+using namespace cb;
+using namespace std;
+
+
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    cerr << "Usage: " << argv[0] << " <input> <output>" << endl;
+    return 1;
+  }
+
+  try {
+    auto in = SystemUtilities::iopen(argv[1], true);
+    auto out = SystemUtilities::oopen(argv[2], 0644, true);
+
+    SystemUtilities::cp(*in, *out);
+  } CATCH_ERROR;
+
+  return 0;
+}
