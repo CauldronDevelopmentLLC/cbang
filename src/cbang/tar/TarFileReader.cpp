1c1dba62bd3e6fa9d0d0c0aa21926043b75382c7
Joseph Coffland <joseph@cauldrondevelopment.com>
2020-07-19 12:43:49 -0700

    Don't allow extraction of tar files outside of the target directory, added tar tests

--- src/cbang/tar/TarFileReader.cpp
+++ src/cbang/tar/TarFileReader.cpp
@@ -101,11 +101,26 @@ std::string TarFileReader::extract(const string &_path) {
   if (!hasMore()) THROW("No more tar files");
 
   string path = _path;
-  if (SystemUtilities::isDirectory(path)) path += "/" + getFilename();
+  if (SystemUtilities::isDirectory(path)) {
+    path += "/" + getFilename();
+
+    // Check that path is under the target directory
+    string a = SystemUtilities::getCanonicalPath(_path);
+    string b = SystemUtilities::getCanonicalPath(path);
+    if (!String::startsWith(b, a))
+      THROW("Tar path points outside of the extraction directory: " << path);
+  }
 
   LOG_DEBUG(5, "Extracting: " << path);
 
-  return extract(*SystemUtilities::oopen(path));
+  switch (getType()) {
+  case NORMAL_FILE: case CONTIGUOUS_FILE:
+    return extract(*SystemUtilities::oopen(path));
+  case DIRECTORY: SystemUtilities::ensureDirectory(path); break;
+  default: THROW("Unsupported tar file type " << getType());
+  }
+
+  return getFilename();
 }
 
 
