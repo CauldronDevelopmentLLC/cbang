37a543ac30dc4eddf30f773d3e6e27d22de82094
Joseph Coffland <joseph@cauldrondevelopment.com>
2020-09-21 16:52:00 -0700

    Added missing file

---
+++ src/cbang/enum/Compression.cpp
@@ -0,0 +1,51 @@
+/******************************************************************************\
+
+          This file is part of the C! library.  A.K.A the cbang library.
+
+                Copyright (c) 2003-2019, Cauldron Development LLC
+                   Copyright (c) 2003-2017, Stanford University
+                               All rights reserved.
+
+         The C! library is free software: you can redistribute it and/or
+        modify it under the terms of the GNU Lesser General Public License
+       as published by the Free Software Foundation, either version 2.1 of
+               the License, or (at your option) any later version.
+
+        The C! library is distributed in the hope that it will be useful,
+          but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+                 Lesser General Public License for more details.
+
+         You should have received a copy of the GNU Lesser General Public
+                 License along with the C! library.  If not, see
+                         <http://www.gnu.org/licenses/>.
+
+        In addition, BSD licensing may be granted on a case by case basis
+        by written permission from at least one of the copyright holders.
+           You may request written permission by emailing the authors.
+
+                  For information regarding this software email:
+                                 Joseph Coffland
+                          joseph@cauldrondevelopment.com
+
+\******************************************************************************/
+
+#define CBANG_ENUM_IMPL
+#include "Compression.h"
+#include <cbang/enum/MakeEnumerationImpl.def>
+#include <cbang/String.h>
+
+using namespace cb;
+using namespace std;
+
+
+Compression CompressionFromPath(const string &path) {
+  if (String::endsWith(path, ".bz2") || String::endsWith(path, ".bzip2"))
+    return Compression::COMPRESSION_BZIP2;
+  if (String::endsWith(path, ".zlib")) return Compression::COMPRESSION_ZLIB;
+  if (String::endsWith(path, ".gz") || String::endsWith(path, ".gzip"))
+    return Compression::COMPRESSION_GZIP;
+  if (String::endsWith(path, ".lz4")) return Compression::COMPRESSION_LZ4;
+
+  return Compression::COMPRESSION_NONE;
+}
