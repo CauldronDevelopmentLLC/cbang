416d8613ea9eb05dc710efe159ae7b2e5b64eacd
Joseph Coffland <joseph@cauldrondevelopment.com>
2022-03-28 01:07:10 +0300

    Support V8 version 10. re:#17

--- src/cbang/js/v8/Context.cpp
+++ src/cbang/js/v8/Context.cpp
@@ -50,7 +50,12 @@ Value Context::eval(const InputSource &src) {
   v8::Local<v8::String> origin;
   string filename = src.getName();
   if (!filename.empty()) origin = Value::createString(filename);
+
+#if V8_MAJOR_VERSION < 10
   v8::ScriptOrigin sOrigin(origin);
+#else
+  v8::ScriptOrigin sOrigin(Value::getIso(), origin);
+#endif
 
   // Get script source
   auto source = Value::createString(src.toString());
