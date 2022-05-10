9bbc91554e1290637df8a466c799ed682e763f61
Joseph Coffland <joseph@cauldrondevelopment.com>
2022-05-10 12:08:18 +0300

    Added missing colon

--- config/cbang/__init__.py
+++ config/cbang/__init__.py
@@ -100,8 +100,7 @@ def configure(conf):
 
     conf.CBRequireLib('cbang-boost')
     conf.CBRequireLib('cbang')
-    if os.uname()[0] == 'FreeBSD'
-        conf.CBRequireLib('sysinfo')
+    if os.uname()[0] == 'FreeBSD': conf.CBRequireLib('sysinfo')
     conf.CBRequireCXXHeader('cbang/Exception.h')
     env.CBDefine('HAVE_CBANG')
 
