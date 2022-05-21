8b7bad9e63205ee103afb0a0df7aea7476b4d341
Thierry Thomas <thierry@FreeBSD.org>
2022-05-21 11:23:33 +0200

    Fix getExecutablePath on FreeBSD

--- src/cbang/os/SystemUtilities.cpp
+++ src/cbang/os/SystemUtilities.cpp
@@ -388,7 +388,7 @@ namespace cb {
       size_t len = PATH_MAX;
       path[0] = path[PATH_MAX] = 0;
 
-      if (sysctl(mib, 4, path, &len, 0, 0) || *path)
+      if (sysctl(mib, 4, path, &len, NULL, 0))
         THROW("Failed to get executable path: " <<  SysError());
 
       return (char *)path;
