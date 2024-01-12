26b8be2c524bfbc7e0cec98d67e14a9edeb8da00
Joseph Coffland <joseph@cauldrondevelopment.com>
2024-01-12 16:55:54 +0200

    Fixes for macOS code

--- src/cbang/os/MacOSUtilities.cpp
+++ src/cbang/os/MacOSUtilities.cpp
@@ -32,168 +32,79 @@
 #ifdef __APPLE__
 
 #include "MacOSUtilities.h"
+#include "MacOSString.h"
+
 #include <sys/sysctl.h>
 
-// The bulk of the get macOS version code comes from a
-// stackoverflow thread http://bit.ly/3pqI8yw
-// https://gist.github.com/kbernhagen/48e89cebac618e0b2bf535e4a1a11afa
+#include <CoreFoundation/CoreFoundation.h>
 
-namespace {
-
-bool versionOK;
-unsigned versions[3];
-
-
-void initMacOSVersionViaSysctl(void * unused) {
-  // try sysctlbyname "kern.osproductversion"; macos 10.13+
-  char str[256];
-  size_t size = sizeof(str);
-  int ret = sysctlbyname("kern.osproductversion", str, &size, 0, 0);
-  if (ret == 0) {
-    int scans = sscanf(&str[0], "%u.%u.%u",
-        &versions[0], &versions[1], &versions[2]);
-    versionOK = (scans >= 2);
-    // accept single-number version if > 0; this could happen in future
-    if ((scans == 1) && (versions[0] > 0))
-      versionOK = true;
-  }
-}
-
-
-void initMacOSVersion(void * unused) {
-  // don't rely on static zero init
-  versionOK = false;
-  versions[0] = versions[1] = versions[2] = 0;
-
-  initMacOSVersionViaSysctl(0);
-  if (versionOK) return;
-
-  // `Gestalt()` actually gets the system version from this file.
-  // `if (@available(macOS 10.x, *))` also gets the version from there.
-  CFURLRef url = CFURLCreateWithFileSystemPath(
-    0, CFSTR("/System/Library/CoreServices/SystemVersion.plist"),
-    kCFURLPOSIXPathStyle, false);
-  if (!url) return;
-
-  CFReadStreamRef readStr = CFReadStreamCreateWithFile(0, url);
-  CFRelease(url);
-  if (!readStr) return;
-
-  if (!CFReadStreamOpen(readStr)) {
-    CFRelease(readStr);
-    return;
-  }
-
-  CFErrorRef outError = 0;
-  CFPropertyListRef propList = CFPropertyListCreateWithStream(
-    0, readStr, 0, kCFPropertyListImmutable, 0, &outError);
-  CFRelease(readStr);
-  if (!propList) {
-    CFShow(outError);
-    CFRelease(outError);
-    return;
-  }
-
-  if (CFGetTypeID(propList) != CFDictionaryGetTypeID()) {
-    CFRelease(propList);
-    return;
-  }
-
-  CFDictionaryRef dict = (CFDictionaryRef)propList;
-  CFTypeRef ver = CFDictionaryGetValue(dict, CFSTR("ProductVersion"));
-  if (ver) CFRetain(ver);
-  CFRelease(dict);
-  if (!ver) return;
-
-  if (CFGetTypeID(ver) != CFStringGetTypeID()) {
-    CFRelease(ver);
-    return;
-  }
-
-  CFStringRef verStr = (CFStringRef)ver;
-  // `1 +` for the terminating NUL (\0) character
-  CFIndex size = 1 + CFStringGetMaximumSizeForEncoding(
-      CFStringGetLength(verStr), kCFStringEncodingASCII);
-  // `calloc` initializes the memory with all zero (all \0)
-  char * cstr = (char *)calloc(1, size);
-  if (!cstr) {
-    CFRelease(verStr);
-    return;
-  }
-
-  CFStringGetBytes(verStr, CFRangeMake(0, CFStringGetLength(verStr)),
-                   kCFStringEncodingASCII, '?', false, (UInt8 *)cstr, size, 0);
-  CFRelease(verStr);
-
-  int scans = sscanf(cstr, "%u.%u.%u",
-      &versions[0], &versions[1], &versions[2]);
-  // There may only be two values, but only one is probably wrong.
-  versionOK = (scans >= 2);
-  // accept single-number version if > 0; this could happen in future
-  if ((scans == 1) && (versions[0] > 0))
-    versionOK = true;
-  free(cstr);
-}
-
-
-} // anonymous namespace
-
+using namespace cb;
 using namespace std;
+
+
+// MacOS version code originates from a stackoverflow thread.
+//
+//   https://stackoverflow.com/questions/11072804
+//   https://gist.github.com/kbernhagen/48e89cebac618e0b2bf535e4a1a11afa
 
 namespace cb {
   namespace MacOSUtilities {
+    Version osVersion;
 
-    bool getMacOSVersions(
-        unsigned *_Nullable outMajor,
-        unsigned *_Nullable outMinor,
-        unsigned *_Nullable outPatch) {
-      static dispatch_once_t onceToken; // MUST be static
-      dispatch_once_f(&onceToken, 0, &initMacOSVersion);
-      if (versionOK) {
-        if (outMajor) *outMajor = versions[0];
-        if (outMinor) *outMinor = versions[1];
-        if (outPatch) *outPatch = versions[2];
-      }
-      return versionOK;
+
+    bool versionBySysCtrl(unsigned ver[3]) {
+      // Try sysctlbyname "kern.osproductversion"; macos 10.13+
+      char str[256];
+      size_t size = sizeof(str);
+
+      return !sysctlbyname("kern.osproductversion", str, &size, 0, 0) &&
+        0 < sscanf(&str[0], "%u.%u.%u", &ver[0], &ver[1], &ver[2]);
     }
 
-    cb::Version getMacOSVersion() {
-      // NOTE: this deliberately never throws an exception
-      unsigned major = 0;
-      unsigned minor = 0;
-      unsigned release = 0;
-      bool ok = getMacOSVersions(&major, &minor, &release);
-      if (!ok) return Version(0, 0, 0);
-      if (major > 255) {
-        major = minor = release = 255;
-      } else if (minor > 255) {
-        minor = release = 255;
-      } else if (release > 255) {
-        release = 255;
-      }
-      return Version((uint8_t)major, (uint8_t)minor, (uint8_t)release);
+
+    bool versionByPList(unsigned ver[3]) {
+      // `Gestalt()` actually gets the system version from this file.
+      // `if (@available(macOS 10.x, *))` also gets the version from there.
+      MacOSRef<CFURLRef> url = CFURLCreateWithFileSystemPath(
+        0, CFSTR("/System/Library/CoreServices/SystemVersion.plist"),
+        kCFURLPOSIXPathStyle, false);
+      if (!url) return false;
+
+      MacOSRef<CFReadStreamRef> stream = CFReadStreamCreateWithFile(0, url);
+      if (!stream || !CFReadStreamOpen(stream)) return false;
+
+      MacOSRef<CFPropertyListRef> props = CFPropertyListCreateWithStream(
+        0, stream, 0, kCFPropertyListImmutable, 0, 0);
+      if (!props || CFGetTypeID(props) != CFDictionaryGetTypeID())
+        return false;
+
+      CFDictionaryRef dict = (CFDictionaryRef)(CFPropertyListRef)props;
+      CFTypeRef verStr =
+        CFDictionaryGetValue(dict, CFSTR("ProductVersion"));
+      if (!verStr) return false;
+      if (CFGetTypeID(verStr) != CFStringGetTypeID()) return false;
+
+      CFRetain(verStr);
+      string vStr = MacOSString((CFStringRef)verStr);
+
+      return 0 < sscanf(vStr.c_str(), "%u.%u.%u", &ver[0], &ver[1], &ver[2]);
     }
 
-    const string toString(const _Nullable CFStringRef cfstr) {
-      if (!cfstr)
-          return string();
-      // CFStringGetCString requires an 8-bit encoding
-      CFStringEncoding encoding = kCFStringEncodingUTF8;
-      const char *str0 = CFStringGetCStringPtr(cfstr, encoding);
-      if (str0)
-          return string(str0);
-      CFIndex len = 1 +
-        CFStringGetMaximumSizeForEncoding(CFStringGetLength(cfstr), encoding);
-      string myString;
-      char *buf = new char[len];
-      if (buf) {
-          if (CFStringGetCString(cfstr, buf, len, encoding))
-              myString = buf;
-          delete[] buf;
-      }
-      return myString;
+
+    void initVersion(void *unused) {
+      unsigned ver[3] = {0, 0, 0};
+
+      if (versionBySysCtrl(ver) || versionByPList(ver))
+        osVersion = Version((uint8_t)ver[0], (uint8_t)ver[1], (uint8_t)ver[2]);
     }
 
+
+    const Version &getOSVersion() {
+      static dispatch_once_t once; // MUST be static
+      dispatch_once_f(&once, 0, &initVersion);
+
+      return osVersion;
+    }
   }
 }
 
