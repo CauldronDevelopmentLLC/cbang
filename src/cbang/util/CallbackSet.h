e870b79119580a948cd7b4b8210dbb98fd4bd3da
Joseph Coffland <joseph@cauldrondevelopment.com>
2025-02-03 15:37:15 +0200

    Added CallbackSet

---
+++ src/cbang/util/CallbackSet.h
@@ -0,0 +1,63 @@
+/******************************************************************************\
+
+          This file is part of the C! library.  A.K.A the cbang library.
+
+                Copyright (c) 2021-2025, Cauldron Development  Oy
+                Copyright (c) 2003-2021, Cauldron Development LLC
+                               All rights reserved.
+
+         The C! library is free software: you can redistribute it and/or
+        modify it under the terms of the GNU Lesser General Public License
+       as published by the Free Software Foundation, either version 2.1 of
+               the License, or (at your option) any later version.
+
+        The C! library is distributed in the hope that it will be useful,
+          but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+                 Lesser General Public License for more details.
+
+         You should have received a copy of the GNU Lesser General Public
+                 License along with the C! library.  If not, see
+                         <http://www.gnu.org/licenses/>.
+
+        In addition, BSD licensing may be granted on a case by case basis
+        by written permission from at least one of the copyright holders.
+           You may request written permission by emailing the authors.
+
+                  For information regarding this software email:
+                                 Joseph Coffland
+                          joseph@cauldrondevelopment.com
+
+\******************************************************************************/
+
+#pragma once
+
+#include <map>
+#include <functional>
+#include <cstdint>
+
+
+namespace cb {
+  template <typename... Args>
+  class CallbackSet {
+  public:
+    using callback_t = std::function<void (Args...)>;
+
+  private:
+    std::map<uint64_t, callback_t> callbacks;
+    uint64_t nextID = 1;
+
+  public:
+    uint64_t add(callback_t cb) {
+      callbacks[nextID] = cb;
+      return nextID++;
+    }
+
+    void remove(uint64_t id) {callbacks.erase(id);}
+
+    void operator()(Args... args) {
+      for (auto &p: callbacks)
+        p.second(args...);
+    }
+  };
+}
